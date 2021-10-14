b9358096e935bae280e7fc020c2b163da82ae0f0
Georgi Kolev <gkolev@mm-sol.com>
2021-10-14 11:43:15 +0300

    Added DemocracyInDanger solution

---
+++ Problems/TimusOnlineJudge/DemocracyInDanger/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+
+    std::priority_queue<int> groups;
+    int kGroups = 0;
+    int minSupporters = 0;
+
+    std::cin >> kGroups;
+
+    for (int temp = 0, i = 0; i <kGroups; i++) {
+        std::cin >> temp;
+        groups.push(-temp);
+    }
+
+    while(groups.size() >= (kGroups / 2 + 1)) {
+        minSupporters += (-groups.top() / 2) + 1;
+        groups.pop();
+    }
+
+    std::cout << minSupporters;
+
+    return 0;
+}
---
+++ Problems/TimusOnlineJudge/README.md
@@ -0,0 +1,3 @@
+These are problems from the website: https://acm.timus.ru/
+
+1025. Democracy in Danger : https://acm.timus.ru/problem.aspx?space=1&num=1025
